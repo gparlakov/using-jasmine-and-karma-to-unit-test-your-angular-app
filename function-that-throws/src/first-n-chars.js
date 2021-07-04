34d9d5be7bc0790db77662b47351694acf59e5c6
gparlakov <gparlakov@gmail.com>
2021-07-04 13:25:35 +0300

    feat(function that throws): finish

--- function-that-throws/src/first-n-chars.js
+++ function-that-throws/src/first-n-chars.js
@@ -4,6 +4,27 @@
  * @param {number} n
  * @returns string
  */
-exports.default = function firstNChars(text, n) {
-  return text.slice(n);
+exports.firstNChars = function firstNChars(text, n) {
+  /**
+   * part 2
+   */
+  if (text === null || text === undefined) {
+    throw new Error('Unexpected "text" type. Expected "string" got: ' + text);
+  }
+  if (typeof text !== 'string') {
+    throw new Error(`Unexpected "text" type. Expected "string" got: ${typeof text}`);
+  }
+  /**
+   * part 3
+   */
+  if (n === null || n === undefined) {
+    throw new Error('Unexpected "n" type. Expected "number" got: ' + n);
+  }
+  if (typeof n !== 'number') {
+    throw new Error(`Unexpected "n" type. Expected "number" got: ${typeof n}`);
+  }
+  /**
+   * part 1
+   */
+  return text.slice(0, n);
 };
--- function-that-throws/src/first-n-chars.spec.js
+++ function-that-throws/src/first-n-chars.spec.js
@@ -1,8 +1,35 @@
-const firstNChars = require('./first-n-chars')
-console.log(firstNChars)
-describe('firstNChars', () => {
-    it('should return first 10 chars from the string', () =>{
-        expect(firstNChars('a veery long string that should be shortened', 10).toEqual('a veery lo'));
-    })
+const { firstNChars } = require('./first-n-chars');
 
+describe('firstNChars', () => {
+  /**
+   * part 1
+   */
+  it('should return first 10 chars from the string', () => {
+    expect(firstNChars('a veery long string that should be shortened', 10)).toEqual('a veery lo');
+  });
+
+  // part 2
+  it('should throw error expected text to be string got null or undefined', () => {
+    expect(() => firstNChars(null, 10)).toThrow(new Error('Unexpected "text" type. Expected "string" got: null'));
+    expect(() => firstNChars(undefined, 10)).toThrow(
+      new Error('Unexpected "text" type. Expected "string" got: undefined')
+    );
+  });
+  it('should throw error expected text to be string got other type', () => {
+    expect(() => firstNChars(1, 10)).toThrow(new Error('Unexpected "text" type. Expected "string" got: number'));
+    expect(() => firstNChars({}, 10)).toThrow(new Error('Unexpected "text" type. Expected "string" got: object'));
+  });
+
+  // part 3
+  it('should throw error n expected number got null or undefined', () => {
+    expect(() => firstNChars('test', null)).toThrow(new Error('Unexpected "n" type. Expected "number" got: null'));
+    expect(() => firstNChars('test', undefined)).toThrow(
+      new Error('Unexpected "n" type. Expected "number" got: undefined')
+    );
+  });
+
+  it('should throw error n expected number got other type', () => {
+    expect(() => firstNChars('test', '1')).toThrow(new Error('Unexpected "n" type. Expected "number" got: string'));
+    expect(() => firstNChars('test', {})).toThrow(new Error('Unexpected "n" type. Expected "number" got: object'));
+  });
 });
